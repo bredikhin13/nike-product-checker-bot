47ce58cdc44fad2fb83fbea8a585d4a10786c3d5
pbredikhin <bredikhin13@gmail.com>
2025-05-25 14:16:25 +0400

    fix jobs

--- infra/dynamodb.tf
+++ infra/dynamodb.tf
@@ -14,4 +14,19 @@ resource "aws_dynamodb_table" "links" {
     type = "S"
   }
 }
+
+resource "aws_dynamodb_table" "tf_lock" {
+  name         = "tf_state_lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
   
